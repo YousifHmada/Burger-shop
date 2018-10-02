5ffcf245b9acb12c38f28db360e06c885aba7f7d
yousifHmada <yousifhmada.moos@gmail.com>
2018-10-02 13:33:37 +0200

    adding main folder definition

---
+++ src/containers/BurgerBuilder/BurgerBulder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux'
+
+
+class BUrgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Cockpit</div>
+            </Aux>
+        )
+    }
+}
+
+export default BUrgerBuilder
